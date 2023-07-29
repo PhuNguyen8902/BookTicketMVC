b4b6f62e6593d8800eda38e81782f6376b23ed22
PhuNguyen8902 <89370742+PhuNguyen8902@users.noreply.github.com>
2023-07-29 16:42:07 +0700

    complete api upload pic

--- be/backend/target/generated-sources/annotations/com/bookticket/pojo/RefeshToken_.java
+++ be/backend/target/generated-sources/annotations/com/bookticket/pojo/RefeshToken_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-29T15:27:50")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-07-29T16:39:13")
 @StaticMetamodel(RefeshToken.class)
 public class RefeshToken_ { 
 
